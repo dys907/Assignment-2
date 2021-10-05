176d5505a0e3df12ec736965e28a5b5ad634563b
MattiasHenders <mhenders1@my.bcit.ca>
2021-10-05 13:28:00 -0700

    Now connects to midp/hits

--- WEB-INF/classes/FileUploadServlet.java
+++ WEB-INF/classes/FileUploadServlet.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.net.*;
 import java.util.Date;
 
 public class FileUploadServlet {
@@ -6,6 +7,9 @@ public class FileUploadServlet {
     private String fileName = null;
     private String dateCreated = null;
     private String keyword = null;
+
+    public Socket socket = null;
+
 
     /**
      * FileUploadServlet Default Constructor
@@ -15,7 +19,8 @@ public class FileUploadServlet {
         fileName = "empty";
         dateCreated = new Date().toString();
         keyword = "";
-        
+
+        setSocket();
     }
 
     /**
@@ -30,6 +35,27 @@ public class FileUploadServlet {
         this.dateCreated = date;
         this.keyword = key;
 
+        setSocket();
+    }
+
+    /**
+     * Sets the socket to upload to UploadServlet at localhost:8081
+     */
+    public void setSocket() {
+        try {
+            socket = new Socket("localhost", 8081);
+        } catch (Exception e) {}
+    }
+
+    /**
+     * Sets the socket to upload to UploadServlet
+     * @param hostName host name ot set
+     * @param portnum port num to set
+     */
+    public void setSocket(String hostName, int portnum) {
+        try {
+            socket = new Socket(hostName, portnum);
+        } catch (Exception e) {}
     }
 
     /**
@@ -40,7 +66,34 @@ public class FileUploadServlet {
         
         System.out.println("Uploading");
 
+        String uploadData = getUploadData();
+
+        DataOutputStream out;
+
+        try {
+
+            out = new DataOutputStream(socket.getOutputStream());
+
+            out.writeBytes("POST /midp/upload HTTP/1.0\r\n\r\n");
+
+            out.writeBytes(uploadData);
+            out.close();
+
+
+        } catch (IOException ioe) {
+            System.out.println("Fail to upload: " + ioe);
+        }
+
         return fileName;
 
     }
+
+    /**
+     * Gets the upload data 
+     * @return data to go to upload stream
+     */
+    private String getUploadData() {
+        return "DATABYTES_" + (fileName + "_" + dateCreated + "_" + keyword).trim();
+    }
+
 }
