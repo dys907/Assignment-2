b3abc6978f8e4bddb915b2a0263589bc13e8abbf
Vicly Xinyue Cao <viclycao1211@gmail.com>
2021-10-10 22:52:02 -0700

    Reflection code

--- WEB-INF/classes/UploadServerThread.java
+++ WEB-INF/classes/UploadServerThread.java
@@ -1,6 +1,9 @@
 import java.net.*;
 import java.io.*;
 import java.time.Clock;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
 public class UploadServerThread extends Thread {
    private Socket socket = null;
    public UploadServerThread(Socket socket) {
@@ -13,9 +16,13 @@ public class UploadServerThread extends Thread {
          HttpRequest req = new HttpRequest(in);
          OutputStream baos = new ByteArrayOutputStream(); 
          HttpResponse res = new HttpResponse(baos);
+         UploadServlet httpServlet = new UploadServlet();
+         Class<?> c = httpServlet.getClass();
+         Method method1 = c.getDeclaredMethod("getServletBaos");
 
-         HttpServlet httpServlet = new UploadServlet();
-         ((UploadServlet) httpServlet).requestHandler(req, res);
+         method1.invoke(httpServlet);
+         Method method2 = c.getDeclaredMethod("requestHandler", HttpRequest.class, HttpResponse.class);
+         method2.invoke(httpServlet, req, res);
 
          ByteArrayOutputStream uploadServletResponse = ((UploadServlet) httpServlet).getServletBaos();
          baos.write(uploadServletResponse.toByteArray());
