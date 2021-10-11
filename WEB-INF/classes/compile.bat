03ce97f2874b1bdf40ca12c9ffa3673b1d38ca92
keyuka <kevyukang@gmail.com>
2021-10-10 23:02:49 -0700

    Remove JSON related compile warnings by including json.jar

--- WEB-INF/classes/compile.bat
+++ WEB-INF/classes/compile.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 
-javac -classpath .;C:\tomcat\lib\json.jar.;C:\tomcat\lib\ojbdc8.jar Activity.java
-javac -classpath .;C:\tomcat\lib\json.jar.;C:\tomcat\lib\ojbdc8.jar UploadClient.java
+javac -classpath .;json.jar.;C:\tomcat\lib\json.jar.;C:\tomcat\lib\ojbdc8.jar Activity.java
+javac -classpath .;json.jar.;C:\tomcat\lib\json.jar.;C:\tomcat\lib\ojbdc8.jar UploadClient.java
 java -classpath .;json.jar Activity
